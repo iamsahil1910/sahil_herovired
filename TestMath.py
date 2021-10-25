a48019b8c5aeb815356180b04aae3c32b2689bbe
Sahil Verma <iamsahil1910@gmail.com>
2021-10-25 18:30:53 +0530

    error

--- TestMath.py
+++ TestMath.py
@@ -5,11 +5,11 @@ class TestMaths(unittest.TestCase):
   def setUp(self):
     self.maths = Maths()
   def test_add(self):
-    self.assertEqual(self.maths.add(4,7), 11)  
+    self.assertEqual(self.maths.add(4,7), 12)  
   def test_subtract(self):
     self.assertEqual(self.maths.subtract(10,5), 5)
   def test_multiply(self):
-    self.assertEqual(self.maths.multiply(3,7), 21)
+    self.assertEqual(self.maths.multiply(3,7), 22)
 
 
 if __name__ == "__main__":
