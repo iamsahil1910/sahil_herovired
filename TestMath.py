7d916a3015cdb62b1d19e4eca091e644240ecb19
Sahil Verma <iamsahil1910@gmail.com>
2021-10-25 11:29:44 +0530

    first commit with test

---
+++ TestMath.py
@@ -0,0 +1,16 @@
+import unittest
+from maths import Maths
+
+class TestMaths(unittest.TestCase):
+  def setUp(self):
+    self.maths = Maths()
+  def test_add(self):
+    self.assertEqual(self.maths.add(4,7), 11)  
+  def test_subtract(self):
+    self.assertEqual(self.maths.subtract(10,5), 5)
+  def test_multiply(self):
+    self.assertEqual(self.maths.multiply(3,7), 21)
+
+
+if __name__ == "__main__":
+  unittest.main()
---
+++ maths.py
@@ -0,0 +1,11 @@
+class Maths:
+
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+  def subtract(self, a, b):
+    return a - b
+
